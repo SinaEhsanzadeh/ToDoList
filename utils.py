2a99165677a9282daca11d7a26b1d43b8e40f20b
Sina Ehsanzadeh <s.eh.81088135@gmail.com>
2025-10-17 23:51:49 +0330

    feat: add is task name taken function

--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ def is_project_name_taken(store, name: str, exclude_id: str = None) -> bool:
         p.name == name and (exclude_id is None or p.id != exclude_id)
         for p in store.list_projects()
     )
+
+def is_task_name_taken(project, name: str, exclude_id: str = None) -> bool:
+    return any(
+        t.name == name and (exclude_id is None or t.id != exclude_id)
+        for t in project.tasks
+    )
